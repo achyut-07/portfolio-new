4a94dcf94759005d314373cedd414b8f5c56b101
achyut-07 <achyutanandsingh9@gmail.com>
2026-03-09 20:15:36 +0530

    ui and bug fixes

---
+++ src/types/gsap-trial-splittext.d.ts
@@ -0,0 +1,19 @@
+declare module "gsap-trial/SplitText" {
+  export interface SplitTextVars {
+    type?: string;
+    linesClass?: string;
+    wordsClass?: string;
+    charsClass?: string;
+    [key: string]: unknown;
+  }
+
+  export class SplitText {
+    chars: Element[];
+    words: Element[];
+    lines: Element[];
+
+    constructor(target: string | Element | Array<string | Element>, vars?: SplitTextVars);
+
+    revert(): void;
+  }
+}
